2c77c02fea5a2bde0caa5adca49817defcbc91d8
kasiabauer <bauer.kasia@gmail.com>
2023-05-06 13:35:20 +0200

    Moving all files from fork o my repository for the book: Python Crash Course.

---
+++ chapter_05/bauer_solved/5.3.aliens.py
@@ -0,0 +1,20 @@
+# alien version 1
+alien_color = 'zielony'
+
+if alien_color == 'zielony':
+    points = 5
+print(f'Gracz uzyskał {points} punktów.')
+
+# alien version 2
+if alien_color == 'zielony':
+    points = 5
+else:
+    points = 0
+print(f'Gracz uzyskał {points} punktów.')
+
+# alien version 3 - failed
+if alien_color == 'czerwony':
+    print(f'Gracz uzyskał 10 punktów.')
+
+
+
